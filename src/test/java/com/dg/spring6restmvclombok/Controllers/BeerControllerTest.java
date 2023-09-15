65e6cbe53966ddebf9b1e0c760de51e527b1986f
Damien Gittings <damiengittings@hotmail.co.uk>
2023-09-15 15:54:08 +0100

    test added for deleteBeerById method within the beer controller

--- src/test/java/com/dg/spring6restmvclombok/Controllers/BeerControllerTest.java
+++ src/test/java/com/dg/spring6restmvclombok/Controllers/BeerControllerTest.java
@@ -6,6 +6,7 @@ import com.dg.spring6restmvclombok.model.Beer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -15,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -92,5 +94,18 @@ class BeerControllerTest {
                 .andExpect(status().isNoContent());
 
         verify(beerService).updateExistingBeer(any(UUID.class), any(Beer.class));
+    }
+    @Test
+    void deleteBeerById() throws Exception {
+        Beer testBeer = beerServiceImp.getBeerList().get(0);
+
+        mockMvc.perform(delete("/api/v1/beer/" + testBeer.getId())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        ArgumentCaptor<UUID> argumentCaptor = ArgumentCaptor.forClass(UUID.class);
+        verify(beerService).deleteBeer(argumentCaptor.capture());
+
+        assertThat(testBeer.getId()).isEqualTo(argumentCaptor.getValue());
     }
 }
